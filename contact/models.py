f137ec9d6316516248c70e93f90fcd895c0cb400
luis-otavio-dias <luis777dias231@gmail.com>
2025-02-25 19:33:59 -0300

    att: visualização da tabela e link para visualização de cada dado

--- contact/models.py
+++ contact/models.py
@@ -27,7 +27,12 @@ class Contact(models.Model):
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, blank=True, null=True
     )
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
