12c87ff31b92f6851b225b8188078a34e430534d
luis-otavio-dias <luis777dias231@gmail.com>
2025-02-25 08:35:10 -0300

    att: add dados na base dados manualmente e automaticamente com utils/create_contacts

--- project/settings.py
+++ project/settings.py
@@ -1,3 +1,5 @@
+# flake8: noqa
+
 """
 Django settings for project project.
 
@@ -104,11 +106,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "pt-br"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Sao_Paulo"
 
-USE_I18N = True
+USE_I18N = False
 
 USE_TZ = True
 
@@ -118,6 +120,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = (BASE_DIR / "base_static",)
+STATIC_ROOT = BASE_DIR / "static"  # collectstatic
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
