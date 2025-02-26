e6f5f395d6cfd5796a3a50d9f084bc05f601fe38
luis-otavio-dias <luis777dias231@gmail.com>
2025-02-26 11:20:14 -0300

    att: corrigido erro de digitação na classe 'table-cel' e pesquisa de valores inexistentes

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from contact.models import Contact
 
 
@@ -9,6 +10,33 @@ def index(request):
     context = {
         "contacts": contacts,
         "site_title": "Contatos - ",
+    }
+
+    return render(request, "contact/index.html", context)
+
+
+def search(request):
+    search_value = request.GET.get("q", "").strip()
+
+    if search_value == "":
+        return redirect("contact:index")
+
+    print(search_value)
+    contacts = (
+        Contact.objects.filter(show=True)
+        .filter(
+            Q(first_name__icontains=search_value)
+            | Q(last_name__icontains=search_value)
+            | Q(phone__icontains=search_value)
+            | Q(email__icontains=search_value)
+        )
+        .order_by("-id")
+    )
+
+    context = {
+        "contacts": contacts,
+        "site_title": "Search - ",
+        "search_value": search_value,
     }
 
     return render(request, "contact/index.html", context)
