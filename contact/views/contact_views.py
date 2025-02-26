4146296851a783d49eb03c761c649b25b54ad243
luis-otavio-dias <luis777dias231@gmail.com>
2025-02-26 07:23:21 -0300

    att: cabeçalho principal(header), partials e title

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -8,6 +8,7 @@ def index(request):
 
     context = {
         "contacts": contacts,
+        "site_title": "Contatos - ",
     }
 
     return render(request, "contact/index.html", context)
@@ -17,9 +18,11 @@ def contact(request, contact_id):
     # single_contact = Contact.objects.filter(pk=contact_id).first()
 
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    contact_name = f"{single_contact.first_name} {single_contact.last_name} - "
 
     context = {
         "contact": single_contact,
+        "site_title": contact_name,
     }
 
     return render(request, "contact/contact.html", context)
