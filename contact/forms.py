68f7e7bf9a5476483a41e8d8f9dd767c6d709bcc
luis-otavio-dias <luis777dias231@gmail.com>
2025-02-27 14:56:52 -0300

    att: campos e widgets do form

--- contact/forms.py
+++ contact/forms.py
@@ -4,27 +4,49 @@ from . import models
 
 
 class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "classe-a classe-b",
+                "placeholder": "Do init",
+            },
+        ),
+        help_text="Texto ajuda",
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # self.fields["first_name"].widget.attrs.update(
+        #     {
+        #         "class": "classe-a classe-b",
+        #         "placeholder": "Type your name",
+        #     }
+        # )
+
     class Meta:
         model = models.Contact
         fields = ("first_name", "last_name", "phone")
+        # widgets = {
+        #     "first_name": forms.TextInput(
+        #         attrs={
+        #             "class": "classe-a classe-b",
+        #             "placeholder": "Type your name",
+        #         },
+        #     ),
+        # }
 
     def clean(self):
         # cleaned_data = self.cleaned_data
 
-        self.add_error(
-            "first_name",
-            ValidationError(
-                "Mensagem de erro",
-                code="invalid",
-            ),
-        )
-
-        self.add_error(
-            "first_name",
-            ValidationError(
-                "Mensagem de erro2",
-                code="invalid",
-            ),
-        )
+        # self.add_error(
+        #     "first_name",
+        #     ValidationError(
+        #         "Mensagem de erro",
+        #         code="invalid",
+        #     ),
+        # )
 
         return super().clean()
+
+    # def
