a6a9d53ddc890ad10b9b0cd9a31f2a0cd762e3c5
luis-otavio-dias <luis777dias231@gmail.com>
2025-02-27 15:22:37 -0300

    att: validando campos do form

--- contact/forms.py
+++ contact/forms.py
@@ -37,16 +37,27 @@ class ContactForm(forms.ModelForm):
         # }
 
     def clean(self):
-        # cleaned_data = self.cleaned_data
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get("first_name")
+        last_name = cleaned_data.get("last_name")
 
-        # self.add_error(
-        #     "first_name",
-        #     ValidationError(
-        #         "Mensagem de erro",
-        #         code="invalid",
-        #     ),
-        # )
+        if first_name == last_name:
+            msg = ValidationError(
+                "Primeiro nome não pode ser igual ao segundo",
+                code="invalid",
+            )
+            self.add_error("first_name", msg)
+            self.add_error("last_name", msg)
 
         return super().clean()
 
-    # def
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get("first_name")
+
+        if first_name == "ABC":
+            raise ValidationError(
+                "Não digite ABC neste campo",
+                code="invalid",
+            )
+
+        return first_name
