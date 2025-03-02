c6eb462ccd64e6943ad483b42ab6f5a2c19a5921
luis-otavio-dias <luis777dias231@gmail.com>
2025-03-02 14:27:13 -0300

    att: Criando registro de usuarios

--- contact/forms.py
+++ contact/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import UserCreationForm
 from . import models
 
 
@@ -52,3 +53,7 @@ class ContactForm(forms.ModelForm):
             )
 
         return first_name
+
+
+class RegisterForm(UserCreationForm):
+    pass
