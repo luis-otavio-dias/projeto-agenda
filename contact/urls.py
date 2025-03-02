f45e16c92b6f9fa871c7ae12e0bd1d272a1f1345
luis-otavio-dias <luis777dias231@gmail.com>
2025-03-02 14:27:13 -0300

    att: Criando registro de ususarios

--- contact/urls.py
+++ contact/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path("contact/create/", views.create, name="create"),
     path("contact/<int:contact_id>/update", views.update, name="update"),
     path("contact/<int:contact_id>/delete", views.delete, name="delete"),
+    # user
+    path("user/create/", views.register, name="register"),
 ]
---
+++ contact/views/user_forms.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+
+def register(request):
+    return render(
+        request,
+        "contact/register.html",
+    )
