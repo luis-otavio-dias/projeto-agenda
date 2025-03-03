7de48222e0fb6947bda43334596c4cac2c15b0c9
luis-otavio-dias <luis777dias231@gmail.com>
2025-03-03 08:22:25 -0300

    att: criado sistema de login e logout

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,6 +1,7 @@
 from contact.forms import RegisterForm
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.forms import AuthenticationForm
 
 
 def register(request):
@@ -12,10 +13,36 @@ def register(request):
         if form.is_valid():
             form.save()
             messages.success(request, "Usuário registrado")
-            return redirect("contact:index")
+            return redirect("contact:login")
 
     context = {
         "form": form,
     }
 
     return render(request, "contact/register.html", context)
+
+
+def login_view(request):
+    form = AuthenticationForm(request)
+
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(request, user)
+            messages.success(request, "Logado com sucesso!")
+            return redirect("contact:index")
+        messages.error(request, "Login inválido")
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "contact/login.html", context)
+
+
+def logout_view(request):
+    auth.logout(request)
+
+    return redirect("contact:login")
